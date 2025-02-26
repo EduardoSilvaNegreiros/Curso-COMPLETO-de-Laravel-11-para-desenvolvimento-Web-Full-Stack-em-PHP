49c67fb9714e0f850eb47366ab9ec190242920dd
Eduardo <edunegreiross@gmail.com>
2025-02-26 12:06:06 -0300

    Efetuando o login

--- notes/app/Http/Controllers/AuthController.php
+++ notes/app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -13,9 +15,54 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+        // form validation
+        $request->validate(
+            // rules
+            [
+                'text_username' => 'required | email',
+                'text_password' => 'required | min:6 | max:16',
+            ],
+            // error messages
+            [
+                'text_username.required' => 'O username é obrigatório!',
+                'text_username.email' => 'Username deve ser um email válido!',
+                'text_password.required' => 'O password é obrigatório!',
+                'text_password.min' => 'O password deve ter pelo menos :min caracteres!',
+                'text_password.max' => 'O password deve ter no máximo :max caracteres!',
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        // check if user exists
+        $user = User::where('username', $username)
+            ->where('deleted_at', NULL)
+            ->first();
+
+        if (!$user) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou password incorretos!');
+        }
+
+        //check if password is correct
+        if (!password_verify($password, $user->password)) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou password incorretos!');
+        }
+
+        // update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        // login user
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+        ]);
+
+        echo 'LOGIN COM SUCESSO!!';
     }
 
     public function logout()
