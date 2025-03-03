26fc86e6882a0c1f1475bdefd4fd65bec1b5a670
Eduardo <edunegreiross@gmail.com>
2025-03-03 11:22:10 -0300

    melhora da descrição em routes

--- notes/routes/web.php
+++ notes/routes/web.php
@@ -6,15 +6,15 @@ use App\Http\Middleware\CheckIsLogged;
 use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-// auth routes - user not logged
+// Rotas para usuários não autenticados (middleware impede acesso se já estiver logado)
 Route::middleware([CheckIsNotLogged::class])->group(function () {
-    Route::get('/login', [AuthController::class, 'login']);
-    Route::post('loginSubmit', [AuthController::class, 'loginSubmit']);
+    Route::get('/login', [AuthController::class, 'login']); // Página de login
+    Route::post('loginSubmit', [AuthController::class, 'loginSubmit']); // Submissão do formulário de login
 });
 
-// app routes - user logged
+// Rotas para usuários autenticados (middleware impede acesso se não estiver logado)
 Route::middleware([CheckIsLogged::class])->group(function () {
-    Route::get('/', [MainController::class, 'index'])->name('home');
-    Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
-    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/', [MainController::class, 'index'])->name('home'); // Página inicial do usuário logado
+    Route::get('/newNote', [MainController::class, 'newNote'])->name('new'); // Página para criar uma nova anotação
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout'); // Logout do usuário
 });
