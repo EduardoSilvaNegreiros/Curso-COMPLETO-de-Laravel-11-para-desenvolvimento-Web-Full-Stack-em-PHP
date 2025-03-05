7c2ac0220ff2e3a3713ebd6ca903d38c4f6c0155
Eduardo <edunegreiross@gmail.com>
2025-03-05 11:56:15 -0300

    Atualização e andamento do projeto

--- notes/app/Http/Controllers/MainController.php
+++ notes/app/Http/Controllers/MainController.php
@@ -2,20 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Services\Operations;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
     public function index()
     {
         // load user's notes
+        $id = session('user.id');
+        $notes = User::Find($id)->notes()->get()->toArray();
+
+
 
         // show home view
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
     {
         echo "I'm creating a new note";
+    }
+
+    public function editNote($id)
+    {
+        $id = Operations::decryptId($id);
+        echo "I'm editing note with id = $id";
+    }
+
+    public function deleteNote($id)
+    {
+
+        $id = Operations::decryptId($id);
+        echo "I'm deleting note with id = $id";
     }
 }
--- notes/app/Http/Middleware/CheckIsLogged.php
+++ notes/app/Http/Middleware/CheckIsLogged.php
@@ -22,4 +22,5 @@ class CheckIsLogged
 
         return $next($request);
     }
+
 }
